fe5ea73fe0d3a904ca07f3f49e2e96353c4d7a0b
Aliane <alianeamaral.tec@gmail.com>
2026-01-14 17:34:29 -0400

    feat: insere exercício 4 de preço total por quantidade

---
+++ exercicio04-preco.py
@@ -0,0 +1,11 @@
+produto = "Cadeira"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print(f"~ Recibo da Loja Ninja ~")
+print(f"Produto: {produto}")
+print(f"Preço Unitário: R$ {preco_unitario}")
+print(f"Quantidade: {quantidade}")
+print(f"Total da Compra: R$ {preco_total:.2f}")

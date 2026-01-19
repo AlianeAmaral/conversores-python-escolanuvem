ae8e33e0109778c86ff674b5f9412ec37a0e7c77
Aliane <alianeamaral.tec@gmail.com>
2026-01-19 14:04:31 -0400

    feat: insere exercicio 9 de cálculo do combustível gasto por percurso

---
+++ atividadepratica2/exercicio09-combustivel.py
@@ -0,0 +1,9 @@
+distanciaPercurso = 300  # km
+combustivel = 25  # litros
+
+consumo_medio = distancia / combustivel
+
+print(f"Distância percorrida: {distancia} km")
+print(f"Combustível gasto: {combustivel} litros")
+
+print(f"O consumo médio desse percurso é: {consumo_medio:.2f} km/l")

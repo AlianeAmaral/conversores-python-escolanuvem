6344d735dd4f580dc72512dcd3b338df9a4ed5b0
Aliane <alianeamaral.tec@gmail.com>
2026-01-19 13:56:15 -0400

    feat: insere exercício 6 de conversor de moeda

---
+++ atividadepratica2/exercicio06-moeda.py
@@ -0,0 +1,10 @@
+valor_reais = 150.00
+taxa_dolar = 5.60
+taxa_euro = 6.60
+
+valor_dolar = valor_reais / taxa_dolar
+valor_euro = valor_reais / taxa_euro
+
+print(f"Valor em reais: R$ {valor_reais:.2f}")
+print(f"Valor em dólares: US$ {valor_dolar:.2f}")
+print(f"Valor em euros: € {valor_euro:.2f}")

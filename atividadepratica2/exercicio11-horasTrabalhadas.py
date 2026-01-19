34ed68e255d5fe7eafb030ac1dd201dce0f254bc
Aliane <alianeamaral.tec@gmail.com>
2026-01-19 14:08:36 -0400

    feat: insere exercicio 11 de cálculo de salário por horas trabalhadas e valor por hora

---
+++ atividadepratica2/exercicio11-horasTrabalhadas.py
@@ -0,0 +1,10 @@
+# salário calculado com base nas horas trabalhadas e valor por hora
+
+matricula_funcionario = int(input())
+horas_trabalhadas = int(input())
+valor_por_hora = float(input())
+
+salario = horas_trabalhadas * valor_por_hora
+
+print(f"Matrícula = {numero_funcionario}")
+print(f"Salário do funcionário = R$ {salario:.2f}")

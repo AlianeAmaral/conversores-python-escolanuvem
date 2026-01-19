1f9e93907f2eb535815ac4b22c0656615b0dec01
Aliane <alianeamaral.tec@gmail.com>
2026-01-19 13:58:25 -0400

    feat: insere exercicio 7 de calculadora de desconto

---
+++ atividadepratica2/exercicio07-desconto.py
@@ -0,0 +1,14 @@
+produto = "Mangá Barakamon Volume nº 01"
+preco_original = 70.00
+desconto_percentual = 20
+
+valor_desconto = preco_original * (desconto_percentual / 100)
+preco_final = preco_original - valor_desconto
+
+print(f"Produto: {produto}")
+print(f"Preço original: R$ {preco_original:.2f}")
+
+print(f"Desconto: {desconto_percentual}%")
+print(f"Valor do desconto: R$ {valor_desconto:.2f}")
+
+print(f"Preço final: R$ {preco_final:.2f}")

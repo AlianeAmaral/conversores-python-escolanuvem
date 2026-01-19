12d55a9b369c8c1ce3787fb153f713ad20c14ace
Aliane <alianeamaral.tec@gmail.com>
2026-01-19 14:01:09 -0400

    feat: insere exercicio 8 de cálculo de média de notas

---
+++ atividadepratica2/exercicio08-notas.py
@@ -0,0 +1,11 @@
+nota1 = 9.5
+nota2 = 8.5
+nota3 = 7.5
+
+media = (nota1 + nota2 + nota3) / 3
+
+print(f"Nota 1: {nota1}")
+print(f"Nota 2: {nota2}")
+print(f"Nota 3: {nota3}")
+
+print(f"A média final do aluno foi: {media:.2f}")
